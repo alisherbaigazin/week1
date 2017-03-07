48d8ec916ad9f9e6f70d04d2ef224ed1430e76d4
alisherbaigazin <alisherbaigazin@gmail.com>
2017-03-07 07:38:58 +0600

    Update Program.cs
    student

--- Student/Student/Program.cs
+++ Student/Student/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,51 +8,41 @@ namespace Student
 {
     class Program
     {
-        //creat a class
         class Student
         {
-            //added data
+          
             public string FirstName;
             public string LastName;
-            public int id;
             public double GPA;
 
-            //creat a constructor with the same name as the class to initialize the data members of the new object
-            public Student(string FirstName, string LastName, int id)
+           
+            public Student(string FirstName, string LastName, double GPA)
             {
-                //"this." to qualify members hidden by similar names
+                
                 this.FirstName = FirstName;
                 this.LastName = LastName;
-                this.id = id;
-            }
-            //creat a constructor for two data members
-            public Student(string FirstName, double GPA)
-            {
-                this.FirstName = FirstName;
                 this.GPA = GPA;
             }
-            //this method use to return a string representation of object
+           
+
             public override string ToString()
             {
-                //return as a string
-                return FirstName + " " + LastName + " " + id + " " + GPA;
+                return FirstName + " " + LastName + " " + GPA;
             }
         }
 
         static void Main(string[] args)
         {
-            //creat a new variables
-            Student a = new Student("Alisher", "Baigazin", 1452);
-            a.GPA = 3.82;
-            Student b = new Student("Akzhan", 4.0);
-            b.LastName = "Ayan";
-            b.id = 5245;
+           
+            Student a = new Student("Alisher", "Baigazin", 3.82);
+            Student b = new Student("Akzhan", "Almukhan", 3.54);
+         
 
-            //display it with method ToString()
+            
             Console.WriteLine(a);
             Console.WriteLine(b);
 
-            //window will not close until we won't press any key
+          
             Console.ReadKey();
         }
     }
